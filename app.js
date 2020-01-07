5a0cfcf2d0d2e8e2f77ecc8fcd4496cc6f67f662
tracer482 <Mustang69>
2020-01-06 20:58:59 -0700

    button not working

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const cameraView = document.querySelector('#camera--view'),
   cameraSensor = document.querySelector('#camera--sensor'),
   cameraTrigger = document.querySelector('#camera--trigger');
 
-cameraTrigger.onClick = function() {
+cameraTrigger.addEventListener('click', () => {
   cameraSensor.width = cameraView.videoWidth;
   cameraSensor.height = cameraView.videoHeight;
   cameraSensor.getContext('2d').drawImage(cameraView, 0, 0);
   cameraOutput.src = cameraSensor.toDataURL('image/webp');
   cameraOutput.classList.add('taken');
-};
+});
 
 function cameraStart() {
   navigator.mediaDevices
